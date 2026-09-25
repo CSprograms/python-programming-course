d247320fcb58b3860b88f0e06a16c7a4041e18bc
CSprograms <78929938+CSprograms@users.noreply.github.com>
2026-09-25 16:04:12 +0530

    Update Program_1_Hello_Python.py

--- Session_Programs/Session_01/Program_1_Hello_Python.py
+++ Session_Programs/Session_01/Program_1_Hello_Python.py
@@ -4,7 +4,7 @@ Session 1 - Program 1: Hello Python
 Write a program to display Hello, Python!.
 
 Sample Output:
-Hello, Python!
+Hello, Python!, by Aravindhan
 """
 
 # Program 1: Display a message
